7b2c0263292aca82cafe63a6e680954aa7207341
Pol Gubau <gubaupol@gmail.com>
2022-07-17 13:20:29 +0200

    ready to edit

--- src/components/TasksGallery/index.tsx
+++ src/components/TasksGallery/index.tsx
@@ -1,16 +1,17 @@
+import { useRouter } from "next/router";
 import { Card } from "semantic-ui-react";
 import { Task } from "src/interfaces/taks";
-
 interface Props {
-  tasks: Task[];
+    tasks: Task[];
 }
 
 export default function TasksGallery({ tasks }: Props) {
+    const router = useRouter()
   return (
     <>
       <Card.Group>
         {tasks.map((task) => (
-          <Card key={task.id}>
+          <Card key={task.id} onClick={()=>{router.push(`/edit/${task.id}`)}}>
             <Card.Content>
               <Card.Header>{task.title}</Card.Header>
               <Card.Meta>{task.description}</Card.Meta>
---
+++ src/pages/edit/[id].tsx
@@ -0,0 +1,15 @@
+
+
+
+
+export default function edit(params:any) {
+    
+   
+  return (
+    <div>
+      <h1>Edit</h1>
+    </div>
+  );
+
+    
+};
